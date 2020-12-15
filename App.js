8e48aff3c6d085c27a6962231247c58fe923386b
Doreen Mensah <doreenradiance@gmail.com>
2020-12-15 13:03:53 +0000

    Navigation Implementing

--- App.js
+++ App.js
@@ -1,21 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'
+
+
+import BiosScreen from "./src/screen/BiosScreen";
+import BioDetails from "./src/screen/BioDetails";
+import SingleScreen from "./src/components/SingleScreen"
+
+const Stack = createStackNavigator()
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerTintColor: "#3b31eb",
+          headerTitleAlign: "center",
+        }}>
+       
+
+       <Stack.Screen
+       options={{
+        header:() =>null
+      }}
+          name='BiosScreen'
+           component={BiosScreen}
+       />
+
+       <Stack.Screen
+        options={{
+          header:() =>null
+        }}
+            name='BioDetails'
+             component={BioDetails}
+       />
+
+
+
+
+      </Stack.Navigator>
+    </NavigationContainer>
+
+    // <View>
+    //   <BiosScreen/>
+    //   {/* <BioDetails/> */}
+    //   {/* <SingleScreen/> */}
+
+    // </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+  // container: {
+  //   flex: 1,
+  //   // backgroundColor: '#fff',
+  //   // alignItems: 'center',
+  //   justifyContent: 'space-around',
+  // },
+// });
